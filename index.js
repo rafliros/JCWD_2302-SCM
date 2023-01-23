922921210d4c98d9f69b7551c66f99d3d22ce56e
rafliros <rafliros06@gmail.com>
2023-01-23 11:17:26 +0700

    Create variable inside index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+// 1. git init (inside project directory) (warning: first initiate only)
+// 2. git add index.js /git add
+// 3. git commit -m "Create variable inside index.js"
+// 4. git log
+// 5. git remote add origin https://github.com/rafliros/JCWD_2302-SCM.git (warning: first initiate only)
+// 6. git push origin master
